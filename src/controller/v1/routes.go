b82f7e2cee5504d7805b517dc833f50966a79273
Sofyan <sofyan.saputra@ralali.com>
2019-11-20 12:39:04 +0700

    Add: Health Check Serviec

--- src/controller/v1/routes.go
+++ src/controller/v1/routes.go
@@ -2,9 +2,12 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	health "github.com/ralali/event-api/src/controller/v1/health"
 	login "github.com/ralali/event-api/src/controller/v1/login"
 	sso "github.com/ralali/event-api/src/controller/v1/sso"
 	users "github.com/ralali/event-api/src/controller/v1/users"
+
+	healthService "github.com/ralali/event-api/src/service/v1/health"
 	loginServices "github.com/ralali/event-api/src/service/v1/login"
 	ssoServices "github.com/ralali/event-api/src/service/v1/sso"
 	"github.com/ralali/event-api/src/util/middleware"
@@ -28,14 +31,29 @@ func (rLoader *V1RouterLoader) V1Router(router *gin.Engine) {
 		SsoService: ssoServices.SsoServiceHandler(),
 	}
 
+	// health
+	// Health Handler Routes
+	healthHandler := &health.V1HealthController{
+		HealthService: healthService.V1HealthCheckHandler(),
+	}
+
 	//********* Calling Handler To Routers *********//
 	rLoader.routerLogin(router, loginHandler)
+	rLoader.routerHealthCheck(router, healthHandler)
 	rLoader.routerOauth(router, ssoHandler)
 	rLoader.routerGoogleOauth(router, ssoHandler)
 
 }
 
 //********* Routing API *********//
+
+// routerDefinition Routes for event organizer | params
+// @router: gin Engine
+// @handler: HealthController
+func (rLoader *V1RouterLoader) routerHealthCheck(router *gin.Engine, handler *health.V1HealthController) {
+	group := router.Group("v1/check")
+	group.GET("", handler.HealthCheck)
+}
 
 // routerDefinition Routes for user | params
 // @router: gin Engine
